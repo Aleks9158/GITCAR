22b01b08e9a127659a83a2150c6a1457518f1267
Aleks9158 <acca5859@gmail.com>
2024-06-21 00:38:04 +0300

    here are changes that you asked me to make up your mind -__-

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,11 @@ public class Main {
     public static void main(String[] args) {
         // the basic  structure
         CarBasics car =new CarBasics();
-<<<<<<< Updated upstream
-        car.color="red";
-        car.engine="7999cc";
-        car.door="2";
-        car.number="119018";
-=======
         car.color="green";
-        car.engine="544444cc";
-        car.door="3";
+        car.engine="54354444cc";
+        car.door="4";
         car.number="789";
->>>>>>> Stashed changes
+
         System.out.println(car.color);
     }
 }
